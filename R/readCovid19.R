09bd030e8f26350f3b185a2e40d03fd18fcbca14
fndemarqui <fndemarqui@gmail.com>
2020-04-20 09:21:44 -0300

    correcting date format again!!!

--- R/readCovid19.R
+++ R/readCovid19.R
@@ -2,6 +2,7 @@
 #'
 #' @description R package to import CODID-19 pandemia data into R. The world level data is downloaded from the Johns Hopkins University's repository. Brazilian data is currently available on states level, and are downloaded from the official Brazilian's repository at https://covid.saude.gov.br/.
 #'
+#'@author Fábio N. Demarqui, Cristiano C. Santos, and Matheus B. Costa.
 #' @docType package
 #' @name readCovid19
 #' @aliases readCovid19
